cd6d552e0e59401292dc692c63c2993343e7b126
Bravinsimiyu <bravinwasike18@gmail.com>
2025-12-15 14:05:27 +0300

    minor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,8 @@ import { Navigate, Route, Routes } from "react-router-dom"
 import HomePage from "./pages/home/HomePage"
 import LoginPage from "./pages/LoginPage"
 import SignUpPage from "./pages/SignUpPage"
-import WatchPage from "./pages/watchPage"
 import Footer from "./components/Footer"
+import WatchPage from "./pages/WatchPage"
 
 import { Toaster } from "react-hot-toast"
 import { useAuthStore } from "./store/authUser"
@@ -13,6 +13,7 @@ import { Loader } from "lucide-react"
 import SearchPage from "./pages/SearchPage"
 import SearchHistoryPage from "./pages/SearchHistoryPage"
 import NotFoundPage from "./pages/404"
+
 
 
 function App() {
