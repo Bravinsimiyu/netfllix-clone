90738e6aaf96f9f5f7a410ee252effc005ed8472
Bravinsimiyu <bravinwasike18@gmail.com>
2025-12-15 13:17:22 +0300

    updated scrollbar css and added loading states when logging in and signing up

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -6,7 +6,7 @@ const LoginPage = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 
-	const { login } = useAuthStore();
+	const { login, isLoggingIn } = useAuthStore();
 
 	const handleLogin = (e) => {
 		e.preventDefault();
@@ -55,8 +55,8 @@ const LoginPage = () => {
 						</div>
 
 						<button
-							className='w-full py-2 bg-red-600 text-white font-semibold rounded-md	hover:bg-red-700'>
-                Login
+							className='w-full py-2 bg-red-600 text-white font-semibold rounded-md	hover:bg-red-700' disabled={isLoggingIn}>
+                {isLoggingIn ? "Loading..." : "Login"}
 						</button>
 					</form>
 					<div className='text-center text-gray-400'>
